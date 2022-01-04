8400504d2860d7bac47e01c5b5e9b3b3a173c918
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2022-01-04 15:06:17 -0500

    create instance connections for users

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 
 const express = require('express');
-const app = express();
 const path = require('path');
 require('dotenv').config();
+
+//express app
+const app = express();
+
+//Node server 
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+//Sockets messagges
+io.on('connection', client => {
+  console.log('Cliente conectado...');
+  client.on('disconnect', () => { console.log('Cliente desconectado') });
+});
 
 
 
 //Public path
 const public = path.resolve(__dirname, 'public');
-
 app.use(express.static(public));
-app.listen(process.env.PORT, (error) => {
+
+server.listen(process.env.PORT, (error) => {
     (error) => {console.log('Error server')};
     console.log('Server is running on port ', process.env.PORT);
 })
