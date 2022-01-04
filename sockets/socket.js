3385365a9e628d693f8f8d77191104a71d75351c
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2022-01-04 17:01:08 -0500

    Emitir evento cuando cliente emite evento

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,10 +1,15 @@
 //Sockets messagges
 const {io} = require('../index')
 io.on('connection', client => {
-    console.log('Cliente conectado...');
+
     client.on('disconnect', () => { console.log('Cliente desconectado') });
       client.on('mensaje', msg => {
         console.log('Mensaje: ', msg);
         io.emit('mensaje', {admin: "Nuevo mensaje"});
       })
+
+    client.on('emitir-mensaje', (payload) =>{
+        console.log('Mensaje emitido: ', payload);
+        io.emit('nuevo-mensaje', payload); //Envía el mensaje a todos los clientes excepto al que lo envía
+    })
   });
